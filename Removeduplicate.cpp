e48ae95b43588a5ee1b8b421e46449b9de01ab1e
Yukesh <147227234+Yukesh2@users.noreply.github.com>
2026-07-03 07:05:01 +0545

    Create Removeduplicate.cpp

---
+++ Removeduplicate.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+   
+    int removeDuplicates(vector<int>& nums) {
+       //this will store the unique values 
+       int slow = 0;
+       //this will scan for unique elements 
+       for(int fast =1;fast<nums.size();fast++){
+        if(nums[slow]!=nums[fast]){
+            slow++;
+            nums[slow] = nums[fast];
+
+        }
+       
+       }
+        return slow +1;
+        }
+    
+};
